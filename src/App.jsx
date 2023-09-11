b31c6cc25546e0bef3db20e77b4acaa65e1c4a17
Kodomo At <guillaume@staub.pro>
2023-09-11 14:32:49 +0200

    Init project with template from Teacher

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList.jsx';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput.jsx';
+import './App.css';
 
-export default App
+const App = () => {
+    const [courseGoals, setCourseGoals] = useState([
+        { text: 'Do all exercises!', id: 'g1' },
+        { text: 'Finish the course!', id: 'g2' }
+    ]);
+
+    const addGoalHandler = enteredText => {
+        setCourseGoals(prevGoals => {
+            const updatedGoals = [...prevGoals];
+            updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+            return updatedGoals;
+        });
+    };
+
+    const deleteItemHandler = goalId => {
+        setCourseGoals(prevGoals => {
+            const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+            return updatedGoals;
+        });
+    };
+
+    let content = (
+        <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+    );
+
+    if (courseGoals.length > 0) {
+        content = (
+            <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+        );
+    }
+
+    return (
+        <div>
+            <section id="goal-form">
+                <CourseInput onAddGoal={addGoalHandler} />
+            </section>
+            <section id="goals">
+                {content}
+                {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+            </section>
+        </div>
+    );
+};
+
+export default App;
+
+
